1957c99982335d70c6955520d9049a366c320314
Joseph Yoon <jyoonopop@gmail.com>
2022-11-02 08:57:19 -0400

    add home page view

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.use('/board', require('./controllers/board_controller'))
 
 app.get ('/', (req, res) => {
-    res.send('Hello world')
+    res.render('home')
 })
 
 app.get('*', (req, res) => {
