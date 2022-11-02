9c11230d942b904b04964034aa359a92fe9b0f07
Joseph Yoon <jyoonopop@gmail.com>
2022-11-02 08:53:33 -0400

    added default view

---
+++ views/default.jsx
@@ -0,0 +1,15 @@
+const React = require('react')
+function Def(html) {
+    return (
+        <html>
+            <head>
+                <title>VG Boards</title>
+            </head>
+            <body>
+                {html.children}
+            </body>
+        </html>
+    )
+}
+
+module.exports = Def
